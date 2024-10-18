f7d60f6fe430224f35ddef45eed67cb26b60c35a
thomaskummer1 <twk23@outlook.com>
2024-10-18 11:49:50 -0400

    3.7 done

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,70 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 export default function CoursesNavigation() {
+  const { pathname } = useLocation();
+  const links = [
+    "Home",
+    "Modules",
+    "Piazza",
+    "Zoom",
+    "Assignments",
+    "Quizzes",
+    "Grades",
+    "People",
+  ];
   return (
-    <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link
-        to="/Kanbas/Courses/1234/Home"
-        id="wd-course-home-link"
-        className="list-group-item active border border-0"
-      >
-        {" "}
-        Home{" "}
-      </Link>
-      <br />
-      <Link
-        to="/Kanbas/Courses/1234/Modules"
-        id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Modules{" "}
-      </Link>
-      <br />
-      <Link
-        to="/Kanbas/Courses/1234/Piazza"
-        id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Piazza{" "}
-      </Link>
-      <br />
-      <Link
-        to="/Kanbas/Courses/1234/Zoom"
-        id="wd-course-zoom-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Zoom{" "}
-      </Link>
-      <br />
-      <Link
-        to="/Kanbas/Courses/1234/Assignments"
-        id="wd-course-quizzes-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Assignments{" "}
-      </Link>
-      <br />
-      <Link
-        to="/Kanbas/Courses/1234/Quizzes"
-        id="wd-course-assignments-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Quizzes{" "}
-      </Link>
-      <br />
-      <Link
-        to="/Kanbas/Courses/1234/People"
-        id="wd-course-people-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        People{" "}
-      </Link>
-      <br />
+    <div>
+      {links.map((link) => (
+        <div
+          id="wd-courses-navigation"
+          className="wd list-group fs-5 rounded-0"
+        >
+          <Link
+            to={link}
+            className={`list-group-item border border-0
+            ${pathname.includes(link) ? "active" : "text-danger"}`}
+          >
+            {link}
+          </Link>
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -25,7 +25,14 @@ import Spreading from "./Spreading";
 import Destructing from "./Destructing";
 import FunctionDestructing from "./FunctionDestructing";
 import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
+import PathParameters from "./PathParameters";
 export default function Lab3() {
+  console.log("Hello World!");
   return (
     <div>
       <h2>Lab 3</h2>
@@ -56,6 +63,14 @@ export default function Lab3() {
       <Destructing />
       <FunctionDestructing />
       <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <br />
+      <Highlight>Highlight this</Highlight>
+      <PathParameters />
     </div>
   );
 }
